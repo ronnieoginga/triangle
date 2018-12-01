4fc30bcc022b6521b6d6d519055f057760e61d2c
ronnieoginga <oginga001ronney@gmail.com>
2018-12-01 19:15:33 +0300

    added var answer

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function result(){
 var side one=parseInt(document.getElementById('side one').value);
 var side two=parseInt(document.getElementById('side two').value);
 var side three=parseInt(document.getElementById('side three').value);
+var answer=document.getElementById('answer');
 }
