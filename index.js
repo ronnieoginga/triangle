66c4410b3160eec3e97b78cb5b3073e65a02161d
ronnieoginga <oginga001ronney@gmail.com>
2018-12-01 19:11:06 +0300

    added var three

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function result(){
 var side one=parseInt(document.getElementById('side one').value);
-var side two=parseInt(document.getElementById('side two').value)
+var side two=parseInt(document.getElementById('side two').value);
+var side three=parseInt(document.getElementById('side three').value);
 }
