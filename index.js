d126fb9e960a09859ea05c513528da7fe8e1687b
ronnieoginga <oginga001ronney@gmail.com>
2018-12-03 11:22:42 +0300

    updated js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var sideone=parseInt(document.getElementById('sideone').value);
 var sidetwo=parseInt(document.getElementById('sidetwo').value);
 var sidethree=parseInt(document.getElementById('sidethree').value);
 var answer=document.querySelector('#answer');
-if(sideone+sidetwo>sidethree && sidetwo+sidethree>sideone && sideone+sidethree>sidetwo) {
+
   if (sideone==sidetwo&&sideone==sidethree) {
     answer.innerHTML="Equilateral";
 }
@@ -11,10 +11,10 @@ else if (sideone===sidetwo||sidetwo===sidethree||sideone===sidethree){
   answer.innerHTML="Isosceles";
 }
 
-else{
+else if(sideone+sidetwo>sidethree || sidetwo+sidethree>=sideone || sideone+sidethree<=sidetwo){
    answer.innerHTML="Scalene";
  }
- 
+
 else{
   answer.innerHTML="Not a triangle";
 }
